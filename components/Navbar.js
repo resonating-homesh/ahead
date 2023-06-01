976544e5554fedb38df140ce5c36b13c56f2fc31
Homesh Nimbarte <75970868+resonating-homesh@users.noreply.github.com>
2023-06-02 03:04:13 +0530

    first commit. developed navbar along with.

---
+++ components/Navbar.js
@@ -0,0 +1,23 @@
+'use client'
+
+const Navbar = () => {
+    
+  return (
+    <div className="flex justify-between p-5 navbar">
+        <div className="logo">
+        <img src="logo1.svg" height={50} width={50} alt="text"/>
+        </div>
+        <div className="links flex justify-between gap-10 m-3 font-semibold">
+        <p>Emotions</p>
+        <p>Manifesto</p>
+        <p>Self-awareness test</p>
+        <p>Work With Us</p>
+        </div>
+        <div className="m-1 bg-black text-white rounded-full justify-center py-3 px-6">
+        <button>  Download app  </button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
